90121d575ce4d6fa5c785bb7d6aa292be267c4bb
Victorino_01 <central.rrodriguez@gmail.com>
2024-05-31 02:03:45 -0300

    Modelos crados en models.py, funciones pre-creadas en views.py, app guardada en settigs.py, migraciones realizadas

---
+++ AppBook/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=40)
+    autor = models.CharField(max_length=60)
+    genero= models.CharField(max_length=25)
+    año_de_publicacion = models.IntegerField()
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    nacionalidad= models.CharField(max_length=25)
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=40)
+    pais= models.CharField(max_length=40)
+    direccion = models.CharField(max_length=50)
---
+++ AppBook/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def libros(req):
+    pass
+
+def autor(req):
+    pass
+
+def editorial(req):
+    pass
